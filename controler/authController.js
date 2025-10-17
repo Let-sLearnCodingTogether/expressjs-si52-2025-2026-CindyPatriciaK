7aee07b00fbd46c1c42936af3219ce97d45294ea
CindyPatriciaK <cindypatriciakadir_2327240008@mhs.mdp.ac.id>
2025-10-17 09:11:54 +0700

    Update authController.js

--- controler/authController.js
+++ controler/authController.js
@@ -5,67 +5,67 @@ export const register = async (req, res) => {
     try {
         //untuk mengambil body atau data dr req
         const registerData = req.body
-        
+
         console.log(registerData);
 
-        console.hashPassword = hash (registerData.password)
+        console.hashPassword = hash(registerData.password)
 
         await UserModel.create({
-            username : registerData.username,
-            email : registerData.email,
-            password : registerData.password
+            username: registerData.username,
+            email: registerData.email,
+            password: registerData.password
         })
 
         res.status(201).json({
-            message : "Berhasil register, silahkan login",
-            data : null
+            message: "Berhasil register, silahkan login",
+            data: null
         })
-    }catch(e){
+    } catch (e) {
         res.status(500).json({
-            message : e.message,
-            data : null
+            message: e.message,
+            data: null
         })
     }
 }
 
-    export const login = async (req,res) => {
-try {
-//untuk mengambil body atau data dari request
-const loginData = req.body
-//mencari user bedasarkan email didatabase
-const user = await UserModel.findOne({
-email : loginData.email
-})
+export const login = async (req, res) => {
+    try {
+        //untuk mengambil body atau data dari request
+        const loginData = req.body
+        //mencari user bedasarkan email didatabase
+        const user = await UserModel.findOne({
+            email: loginData.email
+        })
 
-//jika user tidak ditemukan
-if(!user){
-return res.status(404).json({
-message:"User tidak ditemukan",
-data:null
-})
-}
-//membandingkan password yang ada didalam db dengan request
-if(compare(loginData.password, user.password==loginData.password)){
-res.status(200).json({
-message : "Login Berhasil",
-data : {
-username : user.username,
-email : user.email,
-token : "TOKEN"
-}
-})
-return res.status(401).json({
-message:"Login Tidak Berhasil",
-data:null
-})
-}
-}catch(error){
-res.status(500).json({
-message : error,
-data : null
-})
-}
-
+        //jika user tidak ditemukan
+        if (!user) {
+            return res.status(404).json({
+                message: "User tidak ditemukan",
+                data: null
+            })
+        }
+        //membandingkan password yang ada didalam db dengan request
+        if (compare(loginData.password, user.password == loginData.password)) {
+            res.status(200).json({
+                message: "Login Berhasil",
+                data: {
+                    username: user.username,
+                    email: user.email,
+                    token: "TOKEN"
+                }
+            })
+            return res.status(401).json({
+                message: "Login Tidak Berhasil",
+                data: null
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: error,
+            data: null
+        })
     }
+
+}
 
 
