3c52d725e608edf327880fe8ee3cf3989dd0effa
CindyPatriciaK <cindypatriciakadir_2327240008@mhs.mdp.ac.id>
2025-10-22 20:40:10 +0700

    ss

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,13 @@
 import express from "express"
 import * as authcontroller from "../controler/authController.js"
+import * as profileController from "../controler/profileController.js"
+import { protect } from "../middleware/authMiddleware.js";
 
 const api = express.Router()
 
 api.post ("/register", authcontroller.register)
 api.post ("/login", authcontroller.login)
+
+api.get('/me',protect ,profileController.privateProfile)
 
 export default api 
